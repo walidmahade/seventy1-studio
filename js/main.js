6ade006fb4d92d0cc7f663429aa2944ab6e11ba6
Bd-Dev-007 <56132263+Bd-Dev-007@users.noreply.github.com>
2019-10-28 00:12:49 +0600

    contact page desktop done

--- js/main.js
+++ js/main.js
@@ -9,16 +9,42 @@ const menuShowHide = e => {
     mainMenu.classList.toggle("menu-open");
 };
 
-menuTrigger.addEventListener("click", function (e) {
-    e.preventDefault();
-    menuShowHide(e);
+if (menuTrigger) {
+    menuTrigger.addEventListener("click", function (e) {
+        e.preventDefault();
+        menuShowHide(e);
+    });
+}
+
+if (mainMenu) {
+    document.addEventListener('click', function (e) {
+        let insideMenu = mainMenu.contains(e.target);
+        let insideMenuTrigger = menuTrigger.contains(e.target);
+        if (!insideMenu && !insideMenuTrigger) {
+            menuTrigger.classList.remove('open')
+            mainMenu.classList.remove('menu-open')
+        }
+    })
+}
+
+/*==================================
+        Contact Page
+==================================*/
+const platformSelect = document.querySelectorAll("#target-platforms .options__item");
+
+platformSelect.forEach(elm => {
+    elm.addEventListener("click", (e) => {
+        // console.log(elm);
+        e.preventDefault();
+        let currentItem = e.currentTarget;
+
+        if (currentItem.classList.contains("active")) {
+            currentItem.classList.remove("active")
+            currentItem.children[0].removeAttribute("checked");
+        } else {
+            currentItem.classList.add("active")
+            currentItem.children[0].setAttribute("checked", true);
+        }
+    })
 });
 
-document.addEventListener('click', function (e) {
-    let insideMenu = mainMenu.contains(e.target);
-    let insideMenuTrigger = menuTrigger.contains(e.target);
-    if (!insideMenu && !insideMenuTrigger) {
-        menuTrigger.classList.remove('open')
-        mainMenu.classList.remove('menu-open')
-    }
-})
